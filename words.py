79e15a5bff555e1a910b61234483924d1b320b96
adammgerber <agerber7777@gmail.com>
2022-09-16 04:32:33 -0400

    article function

--- words.py
+++ words.py
@@ -9,6 +9,7 @@ import re
 import spacy
 import icu 
 import pandas as pd
+import json
 
 
 def get_words():
@@ -67,7 +68,34 @@ def get_words():
 
     final_words.to_json('words.json', force_ascii=False, orient='records')
 
+
+def get_article():
+    options = Options()
+    options.headless = True
+    options.add_argument('--disable-gpu')
+    options.add_argument("window-size=1400,600")
+    driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=options)
+    driver.get("https://meduza.io")
+    driver.set_window_size(1920,1080)
+
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.Link-root.Link-isInBlockTitle"))).click()
+
+    #search = driver.find_element(By.CLASS_NAME,"Link-isInBlockTitle").accessible_name
+    #search.click()
+
+    #article = driver.find_element(By.CSS_SELECTOR, "GeneralMaterial-root.GeneralMaterial-simple.GeneralMaterial-default")
+    current_url = driver.current_url
+    link = {"link": f"{current_url}"}
+    driver.quit()
+    
+    # json_object = json.loads(link)
+    with open('link.json', 'w')as f:
+        json.dump(link,f)
+
+
+
 if __name__ == "__main__":
     get_words()
+    get_article()
 
   
